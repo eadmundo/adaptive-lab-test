9e47eb77ff2657e5af6c8cae84de6246b24476b2
Edward Stone <edwardjstone@yahoo.com>
2012-09-30 13:56:54 +0100

    add smoke test for application main page (and .noseexclude)

---
+++ app/blueprints/adaptive/tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from app import create_app
+from app.extensions.db import db
+
+class AdaptiveTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        cls.app = create_app(config='config/test.py')
+        cls.app_test_client = cls.app.test_client()
+
+    def test_main_page_loads(self):
+        """Main page of application returns 200"""
+        rv = self.app_test_client.get('/')
+        self.assertEqual(rv.status_code, 200)
+
+    @classmethod
+    def tearDownClass(cls):
+        #fabfile.db.drop(db_name='test_app', warn=False)
+        pass
